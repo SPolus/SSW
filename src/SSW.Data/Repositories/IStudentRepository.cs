e5ae8fd93a584e488961a306fa91056abd0bd26b
Pavel Solomin <spvspt@gmail.com>
2020-09-11 18:27:27 +0700

    Implement base  and students repository, add empty web project

---
+++ src/SSW.Data/Repositories/IStudentRepository.cs
@@ -0,0 +1,15 @@
+﻿using SSW.Data.Entitties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSW.Data.Repositories
+{
+    public interface IStudentRepository : IAsyncRepository<Student>
+    {
+        Task<Student> GetByIdAsync(int id, bool includeOptions = true);
+        Task<IReadOnlyCollection<Student>> GetAllAsync(bool includeOptions = true);
+    }
+}
