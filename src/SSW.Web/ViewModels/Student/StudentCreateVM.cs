648608906fdc845ecf52e986afd0062da2e6b99f
Pavel Solomin <spvspt@gmail.com>
2020-09-15 10:21:23 +0700

    Some clean up

--- src/SSW.Web/ViewModels/Student/StudentCreateVM.cs
+++ src/SSW.Web/ViewModels/Student/StudentCreateVM.cs
@@ -9,10 +9,25 @@ namespace SSW.Web.ViewModels.Student
     public class StudentCreateVM
     {
         public int Id { get; set; }
+
         [Display(Name = "First Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "First name required")]
         public string FirstName { get; set; }
+
         [Display(Name = "Last Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Last name required")]
         public string LastName { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password required")]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Minimum 6 characters")]
+        public string Password { get; set; }
 
     }
 }
