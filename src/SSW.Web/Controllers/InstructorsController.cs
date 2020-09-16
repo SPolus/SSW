c586cada034925a43e2cfe35ea4c613a42662a24
Pavel Solomin <spvspt@gmail.com>
2020-09-16 16:46:51 +0700

    Start implementing instructors controller

---
+++ src/SSW.Web/Controllers/InstructorsController.cs
@@ -0,0 +1,41 @@
+﻿using SSW.Data.Repositories;
+using SSW.Web.ViewModels.Instructor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SSW.Web.Controllers
+{
+    public class InstructorsController : Controller
+    {
+        private readonly IInstructorRepository _repository;
+
+        public InstructorsController(IInstructorRepository repository)
+        {
+            _repository = repository;
+        }
+        // GET: Instructors
+        public async Task<ActionResult> Index()
+        {
+            var instructors = await _repository.GetAllAsync();
+
+            var results = new List<InstructorIndexVM>();
+
+            foreach (var instructor in instructors)
+            {
+                
+
+                results.Add(new InstructorIndexVM
+                {
+                    Id = instructor.Id,
+                    FullName = $"{instructor.LastName} {instructor.FirstName}",
+                });
+            }
+
+            return View();
+        }
+    }
+}
---
+++ src/SSW.Web/ViewModels/Instructor/CourseStudents.cs
@@ -0,0 +1,18 @@
+﻿using SSW.Data.Entitties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SSW.Web.ViewModels.Instructor
+{
+    public class CourseStudents
+    {
+        [Display(Name = "Course")]
+        public Course Course { get; set; }
+
+        [Display(Name = "Count of students")]
+        public int StudentsCount { get; set; }
+    }
+}
