34118786a068f1edc242f7890bf29077ea992862
Pavel Solomin <spvspt@gmail.com>
2020-09-23 12:20:00 +0700

    Ajax Login

--- src/SSW.Web/Controllers/AccountsController.cs
+++ src/SSW.Web/Controllers/AccountsController.cs
@@ -5,6 +5,7 @@ using System.Web.Mvc;
 using System.Web.Security;
 using SSW.Web.Services;
 using SSW.Data.Entities;
+using System.Threading;
 
 namespace SSW.Web.Controllers
 {
@@ -27,22 +28,6 @@ namespace SSW.Web.Controllers
             return View();
         }
 
-        // [HttpPost]
-        // [ValidateAntiForgeryToken]
-        // public async Task<ActionResult> Login(UserLoginVM user)
-        // {
-        //     var authenticatedUser = await _repository.FirstOrDefaultAsync(x => x.Email == user.Email && x.Password == user.Password, u => new { u.Email });
-        //
-        //     if (authenticatedUser == null)
-        //     {
-        //         ModelState.AddModelError("IncorrectPassword", "Incorrect email or password");
-        //         return View();
-        //     }
-        //
-        //     _cookieService.SetAuthenticationToken(authenticatedUser.Email, user.RememberMe, _cookieTimeoutInMinutes);
-        //     return RedirectToAction("Index", "Home");
-        // }
-        
         [HttpPost]
         public async Task<JsonResult> Login(UserLoginVM user)
         {
@@ -51,11 +36,11 @@ namespace SSW.Web.Controllers
             if (authenticatedUser == null)
             {
                 ModelState.AddModelError("IncorrectPassword", "Incorrect email or password");
-                return Json(new {success = false, responseText = "Incorrect email or password"});
+                return Json(new { success = false, statusCode = 401, statusCodeText = "Unauthorized", responseText = "Incorrect email or password" });
             }
 
             _cookieService.SetAuthenticationToken(authenticatedUser.Email, user.RememberMe, _cookieTimeoutInMinutes);
-            return Json(new {success = true, responseText = ""});
+            return Json(new { success = true,  responseText = "Success" });
         }
 
         [Authorize]
