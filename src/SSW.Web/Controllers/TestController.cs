40e4696a760d7fec775f0068f34d81a69eaa2e7e
Pavel Solomin <spvspt@gmail.com>
2020-09-30 15:40:07 +0700

    30/09/2020

--- src/SSW.Web/Controllers/TestController.cs
+++ src/SSW.Web/Controllers/TestController.cs
@@ -34,25 +34,63 @@ namespace SSW.Web.Controllers
 
             var students = new List<Student>();
 
-            if (f != null)
+            foreach (var s in f)
             {
-                foreach (var s in f)
+                students.Add(new Student
                 {
-                    students.Add(new Student
-                    {
-                        Id = s.Id,
-                        User = new User
-                        {
-                            FirstName = s.FirstName,
-                            LastName = s.LastName,
-                            Email = s.Email,
-                            Password = s.Password
-                        }
-                    });
-                }
+                    Id = s.Id,
+                    User = new User { Id = s.Id, FirstName = s.FirstName, LastName = s.LastName, Email = s.Email, Password = s.Password }
+                });
             }
 
             return View(students);
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> Create(Student student)
+        {
+            bool isExists = await _repository.Exist(x => x.User.Email == student.User.Email);
+
+            if (isExists)
+            {
+                return Json(new { success = false, statusCode = 401, statusCodeText = "Unauthorized", responseText = "Student already exists" });
+            }
+
+            await _repository.AddAsync(student);
+
+            return Json(new { success = true, responseText = "Success" });
+        }
+
+
+        [HttpPost]
+        public async Task<JsonResult> Edit(Student student)
+        {
+            //var st = await _repository.FirstOrDefaultAsync(x => x.User.Email == student.User.Email, s => new { });
+
+            //if (st == null)
+            //{
+            //    return Json(new { success = false, statusCode = 400, statusCodeText = "Bad Request", responseText = "Student doesn't exists" });
+            //}
+
+            //var updated = new Student
+            //{
+            //    Id = st.Id,
+            //    User = new User
+            //    {
+            //        Id = st.Id,
+            //        FirstName = student.User.FirstName,
+            //        LastName = student.User.LastName,
+            //        Email = student.User.Email,
+            //        Password = student.User.Password,
+            //        Student = st.User.Student,
+            //        Instructor = st.User.Instructor
+            //    },
+            //    Enrollments = st.Enrollments
+            //};
+
+            await _repository.UpdateAsync(student);
+
+            return Json(new { success = true, responseText = "Success" });
         }
     }
 }
